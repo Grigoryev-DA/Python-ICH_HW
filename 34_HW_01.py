e1cd0e8dbc5c28bf1fe946fc4d231962727ae726
Grigoryev-DA <alex2012grigoriev@gmail.com>
2024-06-16 23:55:38 +0200

    Update 34_HW_01.py

--- 34_HW_01.py
+++ 34_HW_01.py
@@ -3,7 +3,7 @@ import re
 with open('emails_hw.txt', 'r', encoding="utf-8") as file:
     text = file.read()
 
-text_HW = re.findall("\S{1,}@\w{1,}\.\w{2,}", text)
+text_HW = re.findall("\S{1,}@\w{1,}\.\w{2,}", text)         # не менее 1 любого непробельного символа, @, не менне 1 буквы или цифры, ".", не менне 2 букв или цифер
 print(text_HW)
 
 
