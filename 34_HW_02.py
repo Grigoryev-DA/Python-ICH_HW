2b680ec33219d26cca458f0f0b900c4d6fa05e44
Grigoryev-DA <alex2012grigoriev@gmail.com>
2024-06-17 01:15:19 +0200

    Update 34_HW_02.py

--- 34_HW_02.py
+++ 34_HW_02.py
@@ -1,3 +1,8 @@
+# Напишите функцию highlight_keywords(text, keywords), 
+# которая выделяет все вхождения заданных ключевых слов в тексте,
+#  окружая их символами *. 
+# Функция должна быть регистронезависимой при поиске ключевых слов.
+
 import re
 
 text = "This is a sample text. We need to highlight Python and programming."
@@ -8,6 +13,5 @@ def highlight_keywords(text, keywords):
         words_fin = "".join(re.findall(i, text, re.IGNORECASE))
         text = re.sub(i, f'*{words_fin}*', text, flags=re.IGNORECASE)
     print(text)
-
 
 highlight_keywords(text, keywords)
