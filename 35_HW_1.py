9aa797e5206a4f8640f26985d540596008b8dad7
Grigoryev-DA <alex2012grigoriev@gmail.com>
2024-06-24 00:15:28 +0200

    Update 35_HW_1.py

--- 35_HW_1.py
+++ 35_HW_1.py
@@ -1,3 +1,16 @@
+# 1. Напишите функцию get_response(url), которая отправляет GET-запрос по заданному
+# URL-адресу и возвращает объект ответа. Затем выведите следующую информацию об
+# ответе:
+# - Код состояния (status code)
+# - Текст ответа (response text)
+# - Заголовки ответа (response headers)
+# Пример использования:
+# url = "https://api.example.com"
+# response = get_response(url)
+# print("Status Code:", response.status_code)
+# print("Response Text:", response.text)
+# print("Response Headers:", response.headers)
+
 import requests
 import re
 
